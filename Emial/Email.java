e96142c79f2422d97d27cc34ca2ee0b0cec0cca6
RitaPinto1230205 <123025@isep.ipp.pt>
2024-09-29 18:41:29 +0100

    #3 commit

---
+++ Emial/Email.java
@@ -0,0 +1,18 @@
+package Emial;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String passWord;
+    private String department;
+    private int mailboxcapacity;
+    private String alternativeEmail;
+
+    // construtor que recebe o primeiro e ultimo nome
+
+    // pedir o departamento
+
+    // set a capacidade da mailBox
+
+    // mudar a passWord
+}
