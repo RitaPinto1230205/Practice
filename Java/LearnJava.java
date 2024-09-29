00c5fe11c0375c641b99252abec767454827c59e
RitaPinto1230205 <123025@isep.ipp.pt>
2024-09-29 14:45:18 +0100

    Second commit

--- Java/LearnJava.java
+++ Java/LearnJava.java
@@ -2,9 +2,13 @@ public class LearnJava {
 
     public static void main(String[] args) {
         String name = "Susan";
+        Animal a = new Animal();
 
         System.out.println(name.toLowerCase());
         addExclemationPoint(name);
+
+        String dog = a.iAmDog();
+        System.out.println(dog.toUpperCase());
     }
 
     public static void addExclemationPoint(String s) {
